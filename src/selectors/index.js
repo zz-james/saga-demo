ccc9e1d642c33a449e1703d1be606101888d68d9
James Cat <james@catplusplus.org.uk>
2018-07-24 16:29:21 +0100

    begin selectors module

---
+++ src/selectors/index.js
@@ -0,0 +1,16 @@
+// import combineSelectors from 'combine-selectors';
+// import { getState } from 'todo-redux-state';
+
+// import selectedPage from './site/selected-page';
+// import url from './site/url';
+// import siteHeader from './site/site-header';
+// import todos from './todos/todos';
+
+// const selectors = {
+// 	selectedPage,
+// 	url,
+// 	siteHeader,
+// 	todos
+// };
+
+// export default combineSelectors(selectors, getState);
